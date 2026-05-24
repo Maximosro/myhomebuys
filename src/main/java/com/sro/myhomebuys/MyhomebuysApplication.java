e4365ba3b37d1b3ac64f45376cb8dfebea315482
Maximosro <36531747+Maximosro@users.noreply.github.com>
2026-05-24 13:08:43 +0200

    Initial commit

---
+++ src/main/java/com/sro/myhomebuys/MyhomebuysApplication.java
@@ -0,0 +1,13 @@
+package com.sro.myhomebuys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomebuysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhomebuysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sro/myhomebuys/MyhomebuysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sro.myhomebuys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomebuysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
